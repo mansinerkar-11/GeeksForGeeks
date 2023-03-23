d80b01a5dc4e7ea63d65cb23f079d68e5fdb5cde
Mansi Nerkar <mansinerkar11@gmail.com>
2023-03-23 23:34:04 +0530

    Add files via upload
    Given a string S, we need to find reciprocal of it. The reciprocal of the letter is found by finding the difference between the position of the letter and the first letter ‘A’. Then decreasing the same number of steps from the last letter ‘Z’. The character that we reach after above steps is reciprocal.
    Reciprocal of Z is A and vice versa because if you reverse the position of the alphabet A will be in the position of Z.
    Similarly, if the letter is a small case then the first letter will be 'a' and the last letter will be 'z'. and the definition of reciprocal remains the same.
    
    Note: If the character is not a letter its reciprocal will be the same character.
    
    Example 1:
    
    Input:
    S = "ab C"
    Output:
    zy X
    Explanation:
    The reciprocal of the first letter 'a' is 'z'.
    The reciprocal of the second letter 'b' is 'y'.
    The reciprocal of the third letter ' ' is ' '.
    The reciprocal of the last letter 'C' is 'X'.

---
+++ Program_to_print_reciprocal_of_letters_copy.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Driver function
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    string reciprocalString(string S)
+    {
+        for(int i=0;i<S.size();i++)
+        {
+            if(islower(S[i]))
+                S[i]=97+122-S[i];    
+            else if(isupper(S[i]))
+                S[i]=65+90-S[i];       
+        }
+        return S;
+    
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    string S;
+    cin>>t;
+    cin.ignore();
+ 
+    {
+        getline(cin,S);
+        Solution ob;
+        string reciprocal = ob.reciprocalString(S);
+        cout<< reciprocal <<endl;
+
+    }
+
+	return 0;
+}
+// } Driver Code Ends
