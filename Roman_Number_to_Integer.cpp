04b4e28586bf2a6322c363a94b2c4a04e4c5dc64
Mansi Nerkar <mansinerkar11@gmail.com>
2023-03-27 23:46:51 +0530

    Add files via upload
    Given a string in roman no format (s)  your task is to convert it to an integer . Various symbols and their values are given below.
    I 1
    V 5
    X 10
    L 50
    C 100
    D 500
    M 1000
    
    Example 1:
    
    Input:
    s = V
    Output: 5
    Example 2:
    
    Input:
    s = III 
    Output: 3

---
+++ Roman_Number_to_Integer.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <iostream>
+#include<unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        unordered_map<char, int>mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        
+        
+        int n = str.size();
+        if(n==1)
+        return mp[str[0]];
+        int sum = mp[str[n-1]];
+        for(int i=n-2; i>=0; i--){
+            
+            if(mp[str[i]] >= mp[str[i+1]])
+            sum += mp[str[i]];
+            
+            else{
+                sum -= mp[str[i]];
+            }
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    
+}
+// } Driver Code Ends
