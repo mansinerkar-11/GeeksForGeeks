ae24921eb6bde01959cf619835e6091d6a639a66
Mansi Nerkar <mansinerkar11@gmail.com>
2023-03-27 23:33:02 +0530

    Add files via upload
    Given a N x N matrix, where every row and column is sorted in non-decreasing order. Find the kth smallest element in the matrix.
    
    Example 1:
    Input:
    N = 4
    mat[][] =     {{16, 28, 60, 64},
                       {22, 41, 63, 91},
                       {27, 50, 87, 93},
                       {36, 78, 87, 94 }}
    K = 3
    Output: 27
    Explanation: 27 is the 3rd smallest element.

---
+++ Kth_elementin Matrix.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define MAX 1000
+int mat[MAX][MAX];
+int kthSmallest(int mat[MAX][MAX], int n, int k);
+// driver program to test above function
+int main()
+{
+        int n;
+        cin>>n;
+    
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                cin>>mat[i][j];
+        int r;
+        cin>>r;
+        cout<<kthSmallest(mat,n,r)<<endl;
+    
+     // cout << "7th smallest element is " << kthSmallest(mat, 4, 7);
+      return 0;
+}
+
+// } Driver Code Ends
+
+
+
+int kthSmallest(int mat[MAX][MAX], int n, int k)
+{
+  vector<int>ans;
+  for(int i=0; i<n; i++)
+  {
+      for(int j=0; j<n; j++)
+      {
+          ans.push_back(mat[i][j]);
+      }
+  }
+  sort(ans.begin(), ans.end());
+  return ans[k-1];
+}
