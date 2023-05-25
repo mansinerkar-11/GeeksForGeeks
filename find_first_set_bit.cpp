1f28ac04c99c11dc7fe89f9e3eee36f56ccc935e
Mansi Nerkar <mansinerkar11@gmail.com>
2023-05-25 21:53:27 +0530

    Create find_first_set_bit.cpp

---
+++ find_first_set_bit.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+       if(n==0)return 0;
+        int i=0,k=0;
+        while(true){
+            unsigned int p=n&(1<<k);
+            k++;
+            if(p!=0)return k;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
